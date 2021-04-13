422d24740c47f6625ab5ef6b652cf5db64d690c3
andreytp <andreytp@gmail.com>
2021-04-13 17:15:31 +0300

    change project structure

--- celery_django/wsgi.py
+++ celery_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for celery_setup project.
+WSGI config for celery_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_setup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_django.settings')
 
 application = get_wsgi_application()
